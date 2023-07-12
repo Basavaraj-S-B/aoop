3d2372e8b014618ea50ca72825fe536748b89cd4
Basavaraj-S-B <113363280+Basavaraj-S-B@users.noreply.github.com>
2023-07-12 20:50:44 +0530

    Update and rename two.java to primeException.java

--- primeException.java
+++ primeException.java
@@ -4,16 +4,11 @@ import java.util.*;
 
 
 class NumberNotPrimeException extends Exception {
-
 } 
 
 class NegativeNumberNotAllowedException extends Exception {
 }
-
-
-
-
-class two {
+class primeException {
 	
 	public static void main(String args[]) {
 		
@@ -35,31 +30,24 @@ class two {
             System.out.println("The given number is negative :" +e.toString());
         }
 			
-		try {
-			
-
-				     if(n==0 || n==1)
-		             {
-				           flag=1;
-			         }
-			
-			         for(int i=2;i<=n/2;i++)
-			         {
-				         if(n%i==0)
-				         {
-					        flag=1;
-					        break;
-				         }
-			        }
-			
-			        if(flag==1)
-			        {
-				       throw new NumberNotPrimeException();
-			        }
-			
-			
-	    } catch(NumberNotPrimeException p) {
-            System.out.println("The given number is not prime :" +p.toString());
+		try {			
+		   if(n==0 || n==1)
+   	           {
+		      flag=1;
+		   }
+	           for(int i=2;i<=n/2;i++)
+		   {    if(n%i==0)
+		         {
+			     flag=1;
+	          	     break;
+		         }
+  	           }	
+	           if(flag==1)
+		   {
+		      throw new NumberNotPrimeException();
+		   }
+		} catch(NumberNotPrimeException p) {
+                System.out.println("The given number is not prime :" +p.toString());
         }
     }
 	
